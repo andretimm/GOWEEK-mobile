f765bf11f3245d0f0aefee9abb54b14b31c55044
Andre Timm <andretimm2012@gmail.com>
2018-11-08 12:48:27 -0200

    Criado login

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 
-import { View, Text } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 
 
 export default class Timeline extends Component {
     render() {
         return (
-            <View>
+            <View style={styles.container}>
                 <Text>TimeLine</Text>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#FFF"
+    }
+});  
