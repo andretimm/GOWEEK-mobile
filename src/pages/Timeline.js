3726582a0ba09f0cd0c523b9c7961eaf16d8c65c
Andre Timm <andretimm2012@gmail.com>
2018-11-08 10:46:28 -0200

    Criado paginas e rotas

---
+++ src/pages/Timeline.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import { View, Text } from 'react-native';
+
+
+export default class Timeline extends Component {
+    render() {
+        return (
+            <View>
+                <Text>TimeLine</Text>
+            </View>
+        );
+    }
+}
